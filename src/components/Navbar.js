e475e0f3223f283a12dfcdcd26a0a6ed4394c26e
Ankita <101959776+Asniknigtha@users.noreply.github.com>
2023-02-28 18:03:38 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,147 +1,61 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import MoreIcon from "@mui/icons-material/MoreVert";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import "./style.css";
+
+import "./style.css"
+import React, {useState} from 'react'
+import { NavLink  } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-export default function PrimarySearchAppBar() {
 
+const Navbar = () => {
 
-  let navigate = useNavigate();
+   
+    let navigate = useNavigate();
 
-  const handleLogout = () => 
-  {
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
+    const handleLogout = () => 
+    {
+      localStorage.removeItem("token");
+      navigate("/login");
+    };
 
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-    >
-    </Menu>
-  );
+    return ( 
 
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-    ></Menu>
-  );
-
-  return (
-    <Box sx={{ flexGrow: 1 }} style={{ backgroundColor: "#333739 !important" }}>
-      <AppBar position="static">
-        <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: "none", sm: "block" } }}
-          >
-            iNoteBook
-          </Typography>
-          &nbsp; &nbsp;
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: "none", sm: "block" } }}
-          >
-            <Link to="/home" style={{ textDecorator: "none" }}>
+    <nav>
+      
+        <div>
+          iNotebook
+        </div>
+        <div>
+        <NavLink  to="/home" className="link " exact    >
               {" "}
               Home
-            </Link>
-          </Typography>{" "}
-          &nbsp; &nbsp;
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: "none", sm: "block" } }}
-          >
-            <Link className="active" to="/" selected>
+            </NavLink>
+        </div>
+        <div>
+        <NavLink  className="link"  exact to="/" >
               {" "}
               About
-            </Link>
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
-          {!localStorage.getItem("token") ? (
-            <>
-              <Button>
-                <Link to="/signup">Sign Up</Link>
-              </Button>
-              <Button>
-                <Link to="/login">Login </Link>
-              </Button>
-            </>
+            </NavLink>
+        </div>
+        <div className="blank">
+
+        </div>
+        {!localStorage.getItem("token") ? (
+            <div  >
+              <button className="btn">
+                <NavLink  className="link"  exact  to="/signup">Sign Up</NavLink>
+              </button>
+              <button className="btn">
+                <NavLink  className="link" exact  to="/login">Login </NavLink>
+              </button>
+            </div>
           ) : (
-            <Button onClick={handleLogout}>Log Out</Button>
+            <button className="btn" onClick={handleLogout}>Log Out</button>
 
           )}
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </Box>
-  );
+      
+
+    </nav>
+
+    )
 }
+
+export default Navbar
